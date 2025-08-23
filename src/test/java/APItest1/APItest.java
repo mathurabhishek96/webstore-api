be4c6df714302db5dd9bbf9b7204b44b2820879b
mathurabhishek96 <mathurabhishek73@gmail.com>
2025-08-23 23:35:57 +0100

    testng

--- src/test/java/APItest1/APItest.java
+++ src/test/java/APItest1/APItest.java
@@ -1,20 +1,59 @@
 package APItest1;
 
+import org.testng.annotations.Test;
+
 public class APItest extends APICRUDTests {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+    @Test
+    public void testGet() throws Exception {
+        Get();
+    }
 
-		//Get();
-		//Post();
-		//Put();
-		//Delete();
-		//Posttime();
-		//schema()
-		//pagination();
-		//fileUploaddownload();
-		//downloadFile("https://limewire.com/d/o4tdJ#c1Be8UBPb3", "C:\\Users\\mathu\\OneDrive - Griffith College\\Documents\\test.pdf");
-		datadrivencsv();
-	}
+    @Test
+    public void testPost() throws Exception {
+        Post();
+    }
 
+    @Test
+    public void testPut() throws Exception {
+        Put();
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        Delete();
+    }
+
+    @Test
+    public void testPostTime() throws Exception {
+        Posttime();
+    }
+
+    @Test
+    public void testSchema() throws Exception {
+        schema();
+    }
+
+    @Test
+    public void testPagination() throws Exception {
+        pagination();
+    }
+
+    @Test
+    public void testFileUploadDownload() throws Exception {
+        fileUploaddownload();
+    }
+
+    @Test
+    public void testDownloadFile() throws Exception {
+        downloadFile(
+            "https://limewire.com/d/o4tdJ#c1Be8UBPb3",
+            "C:\\Users\\mathu\\OneDrive - Griffith College\\Documents\\test.pdf"
+        );
+    }
+
+    @Test
+    public void testDataDrivenCSV() throws Exception {
+        datadrivencsv();
+    }
 }
